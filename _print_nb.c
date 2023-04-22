dd2d8882607b1016e112821ea61fdb4efd0e7e43
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-04-22 10:11:09 -0700

    printf

---
+++ _print_nb.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_number - print a given number using _putchar
+ * @n: number to be printed
+ * Return: void
+ */
+int _print_number(int n)
+{
+    int num = 0;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+        num++;
+    }
+
+    if (n / 10)
+        num += _print_number(n / 10);
+
+    _putchar((n % 10) + '0');
+    num++;
+
+    return (num);
+}
---
+++ _printf.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdarg.h>
+
+
+/**
+ * _printf - Write a function that produces output according to a format.
+ * @format: a string containing zero or more format specifiers
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    int i = 0, num = 0;
+    va_list args;
+    va_start(args, format);
+
+    while (format && format[i] != '\0')
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c')
+            {
+                char c = va_arg(args, int);
+                _putchar(c);
+                num++;
+            }
+            else if (format[i] == 's')
+            {
+                int j = 0;
+                char *s = va_arg(args, char *);
+
+                if (s == NULL)
+                    s = "(null)";
+
+                while (s[j])
+                {
+                    _putchar(s[j]);
+                    j++;
+                    num++;
+                }
+            }
+            else if (format[i] == 'd' || format[i] == 'i')
+            {
+                int n = va_arg(args, int);
+                _print_number(n);
+            }
+            else if (format[i] == '%')
+            {
+                _putchar('%');
+                num++;
+            }
+        }
+        else
+        {
+            _putchar(format[i]);
+            num++;
+        }
+        i++;
+    }
+
+    va_end(args);
+    return (num);
+}
