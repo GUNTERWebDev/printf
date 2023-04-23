2c7005ed657b40461b337f8109cc3283ac8ad7a4
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-04-23 07:52:37 -0700

    printf

---
+++ _printf_percent.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf_percent - prints a percent sign
+ * @args: list of arguments
+ * Return: number of characters printed (always 1)
+**/
+int _printf_percent(va_list args)
+{
+	_putchar('%');
+	return (1);
+}
--- main.h
+++ main.h
@@ -8,5 +8,5 @@ int _printf_char(va_list args);
 int _printf_string(va_list args);
 int _printf_number(va_list args);
 int _printf_number(va_list args);
-
+int _printf_percent(va_list args);
 #endif /* MAIN */
