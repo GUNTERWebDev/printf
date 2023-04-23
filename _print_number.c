809abd7816e16dc163ee5c6f0fe3123c33811a6d
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-04-23 15:30:14 -0700

    print

--- _print_number.c
+++ _print_number.c
@@ -1,30 +1,35 @@
 #include "main.h"
 /**
  * _print_number - print a given number using _putchar
- * @n: number to be printed
- * Return: void
+ * @args: argument
+ * Return: len
  */
 int _print_number(va_list args)
 {
 	int n = va_arg(args, int);
 	int len = 0, div = 1;
 
+	if (n == 0)
+		_putchar('0');
 	if (n == -2147483648)
 		_puts("-2147483648");
-	if (n < 0)
+	if (n < 0 && n != -2147483648)
 	{
 		_putchar('-');
 		n = -n;
 		len++;
 	}
-	while (n / div > 9)
-		div *= 10;
-
-	while (div != 0)
+	if (n > 0)
 	{
-		_putchar((n / div) % 10 + '0');
-		div /= 10;
-		len++;
+		while (n / div > 9)
+			div *= 10;
+
+		while (div != 0)
+		{
+			_putchar((n / div) % 10 + '0');
+			div /= 10;
+			len++;
+		}
 	}
 	return (len);
 }
