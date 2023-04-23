464848d85432b150db6c1e69c0495fb736b8fd8d
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-04-23 09:15:07 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -12,6 +12,8 @@ int _printf(const char *format, ...)
 	int (*printf_func)(va_list);
 	va_list args;
 
+	if (format == NULL)
+		return (-1);
 	va_start(args, format);
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
