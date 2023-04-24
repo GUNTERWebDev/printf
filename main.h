8a81b47d0ca5ff760faa525b6e061c4906920861
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-04-24 16:23:59 -0700

    m

--- main.h
+++ main.h
@@ -9,7 +9,7 @@ int _putchar(char c);
 int _print_number(va_list args);
 int _printf_char(va_list args);
 int _printf_string(va_list args);
-int _all(va_list args,char format);
+int _all(va_list args, char format);
 int _uns(va_list args);
 int _hexa(va_list args);
 int _hexaup(va_list args);
