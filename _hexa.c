25020293bbd7fee1e58477af332d74f665229af2
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-04-25 11:18:23 -0700

    printf

--- _hexa.c
+++ _hexa.c
@@ -9,32 +9,38 @@
 int _hexa(va_list args)
 {
 	unsigned int n = va_arg(args, unsigned int);
-	char arr[1024];
+	char hex[] = "0123456789abcdef";
+	char *arr;
 	int i = 0;
 	int j;
+	char swap;
 	unsigned int tmp;
 	
 	if (n == 0)
 	{
 		return (_putchar('0'));
 	}
+	arr = (char*) malloc(sizeof(char) * 20);
+	if (!arr)
+		return (-1);
 	while (n != 0)
 	{
-		tmp = 0;
 		tmp = n % 16;
-		if (tmp < 10)
-		{
-			arr[i] = tmp + '0';
-		}
-		else
-		{
-			arr[i] = tmp + 87;
-		}
-		i++;
+		arr[i++] = hex[tmp];
 		n /= 16;
 	}
-	j = i - 1;
-	while (j >= 0)
-		_putchar(arr[j--]);
+	arr[i] = '\0';
+	j = 0;
+	while (j < i / 2)
+	{
+		swap = arr[j];
+		arr[j] = arr[i - j - 1];
+		arr[i - j - 1] = swap;
+		j++;
+	}
+	j = 0;
+	while (j < i)
+		_putchar(arr[j++]);
+	free(arr);
 	return (i);
 }
